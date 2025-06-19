486361b1f16ab351d120e9e1f534a48a1b32f160
Huy Cao <huy.cao@solenix.ch>
2025-06-19 13:22:54 +0200

    Chapter 5

--- loans/src/test/java/com/banking/loans/LoansApplicationTests.java
+++ loans/src/test/java/com/banking/loans/LoansApplicationTests.java
@@ -3,11 +3,11 @@ package com.banking.loans;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class LoansApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
